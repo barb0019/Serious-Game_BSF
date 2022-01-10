f6cf6ba54394b38fd9f163e4d3f40e62c74e5e28
Jasonjason10 <ofil0001@hz.nl>
2022-01-10 09:19:35 +0100

    update

--- src/ScoringObject.ts
+++ src/ScoringObject.ts
@@ -3,12 +3,13 @@ import GameItem from './GameItem.js';
 export default abstract class ScoringObject extends GameItem {
   private score: number;
 
-
   /**
    *
    * @param imageSrc the src of the image
    * @param maxX the max value of the X position
    * @param maxY the max value of the X position
+   * @param xPos
+   * @param yPos
    * @param score the score of this scoring object
    * @param type the type of tje object
    */
