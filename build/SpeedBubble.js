29a79af37a84fddc1871e22be2a73bf772d15630
schl0014 <schl0014@hz.nl>
2022-01-10 10:39:27 +0100

    update

--- build/SpeedBubble.js
+++ build/SpeedBubble.js
@@ -1,3 +1,8 @@
 export default class SpeedBubble {
+    game;
+    constructor(game, text, yCoordinate, xCoordinate) {
+        this.game = game;
+        this.game.writeTextToCanvas(text, 125, yCoordinate, xCoordinate, 'center', 'red');
+    }
 }
 //# sourceMappingURL=SpeedBubble.js.map
--- src/SpeedBubble.ts
+++ src/SpeedBubble.ts
@@ -1,3 +1,17 @@
-export default class SpeedBubble {
+import Game from './Game.js';
 
+export default class SpeedBubble {
+  private game:Game;
+
+ /**
+  *
+  * @param game
+  * @param text
+  * @param yCoordinate
+  * @param xCoordinate
+  */
+  public constructor(game:Game, text:string, yCoordinate:number, xCoordinate:number) {
+    this.game = game;
+    this.game.writeTextToCanvas(text, 125, yCoordinate, xCoordinate , 'center', 'red');
+  }
 }
