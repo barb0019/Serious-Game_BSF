f7243fbccecf23f57f70d3e0193fc7bfd12e1c92
Jasonjason10 <ofil0001@hz.nl>
2022-01-12 11:04:24 +0100

    update

--- build/VBucks.js
+++ build/VBucks.js
@@ -1,7 +1,18 @@
 import ScoringObject from './ScoringObject.js';
 export default class VBucks extends ScoringObject {
+    timer;
+    flyingSpeed;
     constructor(xPos, yPos, type, points) {
         super(`./assets/img/${type}.png`, xPos, yPos, points, type);
+        this.timer = 0;
+        this.flyingSpeed = 3;
+    }
+    move() {
+        this.xPos += this.flyingSpeed;
+        this.timer += 1;
+        if (this.timer > 20) {
+            this.timer = 0;
+        }
     }
 }
 //# sourceMappingURL=VBucks.js.map
--- src/VBucks.ts
+++ src/VBucks.ts
@@ -1,13 +1,32 @@
 import ScoringObject from './ScoringObject.js';
 
 export default class VBucks extends ScoringObject {
+  private timer: number;
+
+  private flyingSpeed: number;
+
   /**
    *
    * @param xPos the max value of the X position
    * @param yPos the max value of the Y position
    * @param type type of the v buck
+   * @param points
    */
   public constructor(xPos: number, yPos: number, type:string, points:number) {
     super(`./assets/img/${type}.png`, xPos, yPos, points, type);
+
+    this.timer = 0;
+    this.flyingSpeed = 3;
+  }
+
+  /**
+   *
+   */
+  public move():void {
+    this.xPos += this.flyingSpeed;
+    this.timer += 1;
+    if (this.timer > 20) {
+      this.timer = 0;
+    }
   }
 }
