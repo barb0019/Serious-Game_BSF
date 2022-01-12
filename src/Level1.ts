700ff9f656cb269a0e656b225f728bba4542f52a
Jasonjason10 <ofil0001@hz.nl>
2022-01-12 15:00:21 +0100

    update

--- src/Level1.ts
+++ src/Level1.ts
@@ -52,7 +52,7 @@ export default class Level1 extends Level {
    *
    */
   protected objects():void {
-    this.door = new Door(1500, 50, 'DoubleDoor0');
+    this.door = new Door(1400, 50, 'DoubleDoor0');
     this.scoringObjects = [];
     this.scoringObjects.push(new VBucks(790, 160, 'blue', -3, this.game));
     this.scoringObjects.push(new VBucks(1250, 200, 'red', -3, this.game));
@@ -73,7 +73,7 @@ export default class Level1 extends Level {
     const { canvas } = this.game;
     this.platform = [];
     this.platform.push(new Platform(250, 350, 200, 50, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
-    this.platform.push(new Platform(1450, 150, 150, 25, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
+    this.platform.push(new Platform(1350, 150, 150, 25, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
     this.platform.push(new Platform(600, 500, 200, 50, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
     this.platform.push(new Platform(650, 200, 200, 50, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
     this.platform.push(new Platform(1100, 250, 250, 50, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
