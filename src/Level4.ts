8a6b68fcc4e05b799ae51fd5f0d70f32e263536d
Jasonjason10 <ofil0001@hz.nl>
2022-01-17 15:08:09 +0100

    update

--- src/Level4.ts
+++ src/Level4.ts
@@ -64,7 +64,7 @@ export default class Level4 extends Level {
     this.scoringObjects = [];
     this.scoringObjects.push(new VBucks(width * 0.226, height * 0.15, 'blue', -3, this.game));
     this.scoringObjects.push(new VBucks(width * 0.456, height * 0.53, 'blue', -3, this.game));
-    this.scoringObjects.push(new FlyingBuck(width * 0.358, height * 0.2, 'flyingbuck', -3, this.game));
+    this.scoringObjects.push(new FlyingBuck(width * 0.358, height * 0.10, 'flyingbuck', -3, this.game));
     // this.scoringObjects.push(new VBucks(width * 0.15, height * 0.65, 'red', -3, this.game));
     // this.scoringObjects.push(new FutPack(width * 0.2902, height * 0.6, 'blue', -3));
     // this.scoringObjects.push(new FutPack(width * 0.749, height * 0.4, 'red', -3));
@@ -72,6 +72,7 @@ export default class Level4 extends Level {
     this.scoringObjects.push(new Star(width * 0.814, height * 0.45, 'star', 1));
     this.scoringObjects.push(new Star(width * 0.227, height * 0.22, 'star', 1));
     this.scoringObjects.push(new FlyingBuck(width * 0.657, height * 0.30, 'moneymonster', -3, this.game));
+    this.scoringObjects.push(new FlyingBuck(width * 0.657, height * 0.20, 'finalboss', -3, this.game));
     // this.scoringObjects.push(new VBucks(width * 0.179, height * 0.8, 'red', -3, this.game));
     // this.scoringObjects.push(new FutPack(width * 0.273, height * 0.8, 'red', -3));
   }
