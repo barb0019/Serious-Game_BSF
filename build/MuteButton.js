d03c808cdd9b33cd747b94a014387d5ec271c7c7
schl0014 <schl0014@hz.nl>
2022-01-17 14:44:21 +0100

    update

--- build/MuteButton.js
+++ build/MuteButton.js
@@ -2,16 +2,10 @@ import Game from './Game.js';
 import InteractingItems from './InteractingItems.js';
 export default class MuteButton extends InteractingItems {
     constructor(xPos, yPos, type) {
-        super(xPos, yPos, type, './assets/img/ButtonNeutral.png');
-        this.img = Game.loadNewImage('./assets/img/ButtonNeutral.png');
+        super(xPos, yPos, type, './assets/img/UnMute.png');
     }
-    draw(ctx, player) {
-        if (player[0].collidesWith(this) || player[1].collidesWith(this)) {
-            ctx.drawImage(Game.loadNewImage('./assets/img/ButtonEnd.png'), this.xPos, this.yPos, 60, 32);
-        }
-        else {
-            ctx.drawImage(this.img, this.xPos, this.yPos, 60, 32);
-        }
+    draw(ctx) {
+        ctx.drawImage(Game.loadNewImage('./assets/img/UnMute.png'), this.xPos, this.yPos, 60, 32);
     }
 }
 //# sourceMappingURL=MuteButton.js.map
--- src/MuteButton.ts
+++ src/MuteButton.ts
@@ -5,24 +5,19 @@ import Player from './Player.js';
 export default class MuteButton extends InteractingItems {
   /**
    *
-   * @param xPos x position of the door
+   * @param xPos x position of the mute button
    * @param yPos y position of the door
    * @param type type of the door
    */
   public constructor(xPos: number, yPos: number, type:string) {
-    super(xPos, yPos, type, './assets/img/ButtonNeutral.png');
-    this.img = Game.loadNewImage('./assets/img/ButtonNeutral.png');
+    super(xPos, yPos, type, './assets/img/UnMute.png');
   }
 
   /**
    * @param ctx ctx
    * @param player the array of players
    */
-  public draw(ctx: CanvasRenderingContext2D, player:Player[]): void {
-    if (player[0].collidesWith(this) || player[1].collidesWith(this)) {
-      ctx.drawImage(Game.loadNewImage('./assets/img/ButtonEnd.png'), this.xPos, this.yPos, 60, 32);
-    } else {
-      ctx.drawImage(this.img, this.xPos, this.yPos, 60, 32);
-    }
+  public draw(ctx: CanvasRenderingContext2D,): void {
+    ctx.drawImage(Game.loadNewImage('./assets/img/UnMute.png'), this.xPos, this.yPos, 60, 32);
   }
 }
