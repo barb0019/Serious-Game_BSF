2cdfb7f0c700286e2c4409e10222a7ca1e9604ee
Mitchel0058 <goed0058@hz.nl>
2022-01-17 14:01:14 +0100

    The boss now also doesn't go farther when the speed increases

--- build/VBucks.js
+++ build/VBucks.js
@@ -1,17 +1,26 @@
 import Enemies from './Enemies.js';
 export default class VBucks extends Enemies {
-    timer;
+    maxXPos;
+    goingRight;
     constructor(xPos, yPos, type, points, game) {
         super(`./assets/img/${type}.png`, xPos, yPos, points, type, false, game);
-        this.timer = 0;
+        this.goingRight = false;
+        this.maxXPos = this.xPos - game.canvas.width * 0.28;
         this.flyingSpeed += 4;
     }
     move() {
         this.xPos -= this.flyingSpeed;
-        this.timer += 1;
-        if (this.timer > 115) {
-            this.timer = 0;
+        if (this.goingRight) {
+            if (this.xPos > this.maxXPos) {
+                this.maxXPos = this.xPos - this.game.canvas.width * 0.28;
+                this.flyingSpeed = -this.flyingSpeed;
+                this.goingRight = false;
+            }
+        }
+        else if (this.xPos < this.maxXPos) {
+            this.maxXPos = this.xPos + this.game.canvas.width * 0.28;
             this.flyingSpeed = -this.flyingSpeed;
+            this.goingRight = true;
         }
     }
 }
