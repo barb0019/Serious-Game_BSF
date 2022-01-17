0a559179a1ca31c5f41488067221159e8b8a7bf6
schl0014 <schl0014@hz.nl>
2022-01-17 14:35:38 +0100

    update

---
+++ src/MuteButton.ts
@@ -0,0 +1,28 @@
+import Game from './Game.js';
+import InteractingItems from './InteractingItems.js';
+import Player from './Player.js';
+
+export default class MuteButton extends InteractingItems {
+  /**
+   *
+   * @param xPos x position of the door
+   * @param yPos y position of the door
+   * @param type type of the door
+   */
+  public constructor(xPos: number, yPos: number, type:string) {
+    super(xPos, yPos, type, './assets/img/ButtonNeutral.png');
+    this.img = Game.loadNewImage('./assets/img/ButtonNeutral.png');
+  }
+
+  /**
+   * @param ctx ctx
+   * @param player the array of players
+   */
+  public draw(ctx: CanvasRenderingContext2D, player:Player[]): void {
+    if (player[0].collidesWith(this) || player[1].collidesWith(this)) {
+      ctx.drawImage(Game.loadNewImage('./assets/img/ButtonEnd.png'), this.xPos, this.yPos, 60, 32);
+    } else {
+      ctx.drawImage(this.img, this.xPos, this.yPos, 60, 32);
+    }
+  }
+}
