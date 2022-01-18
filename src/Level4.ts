984185457b4f2df12c9fe26e5cf5db77f80a20a9
Jasonjason10 <ofil0001@hz.nl>
2022-01-18 10:28:26 +0100

    update

--- src/Level4.ts
+++ src/Level4.ts
@@ -64,11 +64,11 @@ export default class Level4 extends Level {
     this.door = new Door(width * 0.911, height * 0.018, 'DoubleDoor0');
     this.scoringObjects = [];
     this.scoringObjects.push(new VBucks(width * 0.226, height * 0.22, 'blue', -3, this.game));
-    this.scoringObjects.push(new FutPack(width * 0.456, height * 0.53, 'blue', -3));
+    this.scoringObjects.push(new FutPack(width * 0.456, height * 0.5, 'blue', -3));
     this.scoringObjects.push(new Shootingbuck(width * 0.358, height * 0.21, 'red', -3, this.game));
     this.scoringObjects.push(new FlyingBuck(width * 0.657, height * 0.155, 'moneymonster', -3, this.game));
     this.scoringObjects.push(new FlyingBuck(width * 0.75, height * 0.21, 'finalboss', -3, this.game));
-    this.scoringObjects.push(new FutPack(width * 0.556, height * 0.53, 'red', -3));
+    this.scoringObjects.push(new FutPack(width * 0.556, height * 0.5, 'red', -3));
     // this.scoringObjects.push(new VBucks(width * 0.15, height * 0.65, 'red', -3, this.game));
     // this.scoringObjects.push(new FutPack(width * 0.2902, height * 0.6, 'blue', -3));
     // this.scoringObjects.push(new FutPack(width * 0.749, height * 0.4, 'red', -3));
