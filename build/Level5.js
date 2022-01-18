08aceb07487b0b206b5593fffab499a926f6cf99
Jasonjason10 <ofil0001@hz.nl>
2022-01-18 13:12:55 +0100

    update

--- build/Level5.js
+++ build/Level5.js
@@ -3,13 +3,9 @@ import Platform from './Platform.js';
 import VBucks from './VBucks.js';
 import PlayerRed from './PlayerRed.js';
 import PlayerBlue from './PlayerBlue.js';
-import FutPack from './FutPack.js';
-import Star from './Star.js';
 import Door from './Door.js';
-import FlyingBuck from './FlyingBuck.js';
 import SpeedBubble from './SpeedBubble.js';
 import Level from './Level.js';
-import Shootingbuck from './ShootingBucks.js';
 export default class Level5 extends Level {
     constructor(game) {
         super(game);
@@ -33,27 +29,12 @@ export default class Level5 extends Level {
         this.door = new Door(width * 0.911, height * 0.018, 'DoubleDoor0');
         this.scoringObjects = [];
         this.scoringObjects.push(new VBucks(width * 0.226, height * 0.22, 'blue', -3, this.game));
-        this.scoringObjects.push(new FutPack(width * 0.41, height * 0.5, 'blue', -3));
-        this.scoringObjects.push(new Shootingbuck(width * 0.358, height * 0.21, 'red', -3, this.game));
-        this.scoringObjects.push(new FlyingBuck(width * 0.657, height * 0.155, 'moneymonster', -3, this.game));
-        this.scoringObjects.push(new FlyingBuck(width * 0.75, height * 0.2, 'finalboss', -3, this.game));
-        this.scoringObjects.push(new FutPack(width * 0.51, height * 0.5, 'red', -3));
-        this.scoringObjects.push(new FutPack(width * 0.61, height * 0.5, 'blue', -3));
-        this.scoringObjects.push(new Star(width * 0.84, height * 0.15, 'star', 1));
-        this.scoringObjects.push(new Star(width * 0.814, height * 0.45, 'star', 1));
-        this.scoringObjects.push(new Star(width * 0.227, height * 0.15, 'star', 1));
     }
     makePlatforms() {
         const { width } = this.game.canvas;
         const { height } = this.game.canvas;
         console.log(height);
         this.platform = [];
-        this.platform.push(new Platform(width * 0.163, height * 0.28, width * 0.13, 50, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
-        this.platform.push(new Platform(width * 0.9115, height * 0.154, width * 0.048, 25, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
-        this.platform.push(new Platform(width * 0.130, height * 0.77, width * 0.195, 50, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
-        this.platform.push(new Platform(width * 0.350, height * 0.6, width * 0.2, 50, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
-        this.platform.push(new Platform(width * 0.39, height * 0.28, width * 0.19, 50, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
-        this.platform.push(new Platform(width * 0.75, height * 0.28, width * 0.13, 50, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
         console.log('LEVEL2');
         this.platform.push(new Platform(0, height - 50, width / 4, 50, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
         this.platform.push(new Platform(width / 4, height - 50, width / 4, 50, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
@@ -61,10 +42,6 @@ export default class Level5 extends Level {
         this.platform.push(new Platform(width * 0.75, height - 50, width / 4, 50, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
     }
     allMove() {
-        this.scoringObjects[2].move();
-        this.scoringObjects[3].moveY();
-        this.platform[3].moveX();
-        this.platform[0].moveY();
     }
 }
 //# sourceMappingURL=Level5.js.map
--- src/Level5.ts
+++ src/Level5.ts
@@ -64,18 +64,18 @@ export default class Level5 extends Level {
     this.door = new Door(width * 0.911, height * 0.018, 'DoubleDoor0');
     this.scoringObjects = [];
     this.scoringObjects.push(new VBucks(width * 0.226, height * 0.22, 'blue', -3, this.game));
-    this.scoringObjects.push(new FutPack(width * 0.41, height * 0.5, 'blue', -3));
-    this.scoringObjects.push(new Shootingbuck(width * 0.358, height * 0.21, 'red', -3, this.game));
-    this.scoringObjects.push(new FlyingBuck(width * 0.657, height * 0.155, 'moneymonster', -3, this.game));
-    this.scoringObjects.push(new FlyingBuck(width * 0.75, height * 0.2, 'finalboss', -3, this.game));
-    this.scoringObjects.push(new FutPack(width * 0.51, height * 0.5, 'red', -3));
-    this.scoringObjects.push(new FutPack(width * 0.61, height * 0.5, 'blue', -3));
-    // this.scoringObjects.push(new VBucks(width * 0.15, height * 0.65, 'red', -3, this.game));
-    // this.scoringObjects.push(new FutPack(width * 0.2902, height * 0.6, 'blue', -3));
-    // this.scoringObjects.push(new FutPack(width * 0.749, height * 0.4, 'red', -3));
-    this.scoringObjects.push(new Star(width * 0.84, height * 0.15, 'star', 1));
-    this.scoringObjects.push(new Star(width * 0.814, height * 0.45, 'star', 1));
-    this.scoringObjects.push(new Star(width * 0.227, height * 0.15, 'star', 1));
+    // this.scoringObjects.push(new FutPack(width * 0.41, height * 0.5, 'blue', -3));
+    // this.scoringObjects.push(new Shootingbuck(width * 0.358, height * 0.21, 'red', -3, this.game));
+    // this.scoringObjects.push(new FlyingBuck(width * 0.657, height * 0.155, 'moneymonster', -3, this.game));
+    // this.scoringObjects.push(new FlyingBuck(width * 0.75, height * 0.2, 'finalboss', -3, this.game));
+    // this.scoringObjects.push(new FutPack(width * 0.51, height * 0.5, 'red', -3));
+    // this.scoringObjects.push(new FutPack(width * 0.61, height * 0.5, 'blue', -3));
+    // // this.scoringObjects.push(new VBucks(width * 0.15, height * 0.65, 'red', -3, this.game));
+    // // this.scoringObjects.push(new FutPack(width * 0.2902, height * 0.6, 'blue', -3));
+    // // this.scoringObjects.push(new FutPack(width * 0.749, height * 0.4, 'red', -3));
+    // this.scoringObjects.push(new Star(width * 0.84, height * 0.15, 'star', 1));
+    // this.scoringObjects.push(new Star(width * 0.814, height * 0.45, 'star', 1));
+    // this.scoringObjects.push(new Star(width * 0.227, height * 0.15, 'star', 1));
     // this.scoringObjects.push(new VBucks(width * 0.179, height * 0.8, 'red', -3, this.game));
     // this.scoringObjects.push(new FutPack(width * 0.273, height * 0.8, 'red', -3));
   }
@@ -90,13 +90,13 @@ export default class Level5 extends Level {
 
     this.platform = [];
 
-    this.platform.push(new Platform(width * 0.163, height * 0.28, width * 0.13, 50, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
-    this.platform.push(new Platform(width * 0.9115, height * 0.154, width * 0.048, 25, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
-    this.platform.push(new Platform(width * 0.130, height * 0.77, width * 0.195, 50, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
-    this.platform.push(new Platform(width * 0.350, height * 0.6, width * 0.2, 50, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
-    // this.platform.push(new Platform(width * 0.75, height * 0.5, width * 0.13, 50, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
-    this.platform.push(new Platform(width * 0.39, height * 0.28, width * 0.19, 50, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
-    this.platform.push(new Platform(width * 0.75, height * 0.28, width * 0.13, 50, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
+    // this.platform.push(new Platform(width * 0.163, height * 0.28, width * 0.13, 50, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
+    // this.platform.push(new Platform(width * 0.9115, height * 0.154, width * 0.048, 25, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
+    // this.platform.push(new Platform(width * 0.130, height * 0.77, width * 0.195, 50, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
+    // this.platform.push(new Platform(width * 0.350, height * 0.6, width * 0.2, 50, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
+    // // this.platform.push(new Platform(width * 0.75, height * 0.5, width * 0.13, 50, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
+    // this.platform.push(new Platform(width * 0.39, height * 0.28, width * 0.19, 50, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
+    // this.platform.push(new Platform(width * 0.75, height * 0.28, width * 0.13, 50, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
 
     console.log('LEVEL2');
     // the ground
@@ -107,10 +107,10 @@ export default class Level5 extends Level {
   }
 
   public allMove(): void {
-    this.scoringObjects[2].move();
-    this.scoringObjects[3].moveY();
-    this.platform[3].moveX();
-    this.platform[0].moveY();
+    // this.scoringObjects[2].move();
+    // this.scoringObjects[3].moveY();
+    // this.platform[3].moveX();
+    // this.platform[0].moveY();
     // this.platform[5].moveY();
     // this.scoringObjects[1].move();
   }
