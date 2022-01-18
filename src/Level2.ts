d19b75b90abe6e0439a99695f836ec4c6a41705f
barb0019 <barb0019@hz.nl>
2022-01-18 14:22:37 +0100

    b
    bb

--- src/Level2.ts
+++ src/Level2.ts
@@ -41,7 +41,7 @@ export default class Level2 extends Level {
    */
   protected speedbubbles(game: Game): void {
     this.speedBubble = [];
-    this.speedBubble.push(new SpeedBubble(game, 'dit is een vliegende vbuck pas dus op', 120, 600, 100, 500));
+    this.speedBubble.push(new SpeedBubble(game, 'dit is een vliegende vbuck ! Pas dus op.', 120, 600, 100, 500));
   }
 
   /**
