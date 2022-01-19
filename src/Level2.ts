42f818e8c634ba44a7d172589aac466b0c47ff0c
Jasonjason10 <ofil0001@hz.nl>
2022-01-19 14:53:22 +0100

    update

--- src/Level2.ts
+++ src/Level2.ts
@@ -97,10 +97,10 @@ export default class Level2 extends Level {
 
     // creates the top left platform
     this.platform.push(new Platform(width * 0.163, height * 0.28, width * 0.13, 50, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
-    //
+    // creates the platform on the bottom left
     this.platform.push(new Platform(width * 0.9115, height * 0.154, width * 0.048, 25, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
-    //
-    // this.platform.push(new Platform(width * 0.130, height * 0.7, width * 0.195, 50, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
+    // creates the bottom left platform
+    this.platform.push(new Platform(width * 0.130, height * 0.7, width * 0.195, 50, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
     // creates the bottom middle platform
     this.platform.push(new Platform(width * 0.400, height * 0.6, width * 0.2, 50, Game.loadNewImage('./assets/img/TileMapDesert2.png')));
     // creates the top middle platfrom
