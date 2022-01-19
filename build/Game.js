1823066eba023486845df38eff73c30ec91aa479
RobinvLa <70522569+RobinvLa@users.noreply.github.com>
2022-01-19 13:29:07 +0100

    boost

--- build/Game.js
+++ build/Game.js
@@ -1,4 +1,5 @@
 import GameLoop from './GameLoop.js';
+import Level1 from './Level1.js';
 import Level2 from './Level2.js';
 import Start from './Start.js';
 import UserData from './UserData.js';
@@ -67,7 +68,7 @@ export default class Game {
     }
     getCurrentLevel() {
         switch (this.user.getLevel()) {
-            case 1: return new Level5(this);
+            case 1: return new Level1(this);
             case 2: return new Level2(this);
             case 3: return new Level3(this);
             case 4: return new Level4(this);
