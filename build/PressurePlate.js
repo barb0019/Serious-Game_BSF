aa6dca95336859270caba1ffc3934cc5504cc28c
schl0014 <schl0014@hz.nl>
2022-01-20 14:50:53 +0100

    pressure plate

--- build/PressurePlate.js
+++ build/PressurePlate.js
@@ -2,12 +2,12 @@ import Game from './Game.js';
 import InteractingItems from './InteractingItems.js';
 export default class PressurePlate extends InteractingItems {
     constructor(xPos, yPos, type) {
-        super(xPos, yPos, type, './assets/img/ButtonNeutralYellow.png');
-        this.img = Game.loadNewImage('./assets/img/ButtonNeutralYellow.png');
+        super(xPos, yPos, type, './assets/img/interactingitems/ButtonNeutralYellow.png');
+        this.img = Game.loadNewImage('./assets/img/interactingitems/ButtonNeutralYellow.png');
     }
     draw(ctx, player) {
         if (player[0].collidesWith(this) || player[1].collidesWith(this)) {
-            ctx.drawImage(Game.loadNewImage('./assets/img/ButtonEndForest.png'), this.xPos, this.yPos, 60, 32);
+            ctx.drawImage(Game.loadNewImage('./assets/img/interactingitems/ButtonEndForest.png'), this.xPos, this.yPos, 60, 32);
         }
         else {
             ctx.drawImage(this.img, this.xPos, this.yPos, 60, 32);
