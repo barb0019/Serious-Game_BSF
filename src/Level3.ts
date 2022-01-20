3fd8ec03c5eb57b221ca5cf770f69be7c23b5f44
barb0019 <barb0019@hz.nl>
2022-01-20 13:32:40 +0100

    bb
    bb

--- src/Level3.ts
+++ src/Level3.ts
@@ -35,9 +35,9 @@ export default class Level3 extends Level {
    */
   protected speedbubbles(game: Game):void {
     this.speedBubble = [];
-    this.speedBubble.push(new SpeedBubble(game, 'Zijn in-game aankopen je geld wel waard?', 520, 290, 100, 500));
-    this.speedBubble.push(new SpeedBubble(game, 'Ookal zijn games zoals fortnite gratis geven 10-12 jarige er veel geld aan uit  ', 520, 290, 100, 500));
-    this.speedBubble.push(new SpeedBubble(game, 'Zijn alle in-game aankopen slecht', 520, 290, 100, 500));
+    this.speedBubble.push(new SpeedBubble(game, 'Zijn alle in-game aankopen je geld wel waard?', 520, 290, 100, 500));
+    this.speedBubble.push(new SpeedBubble(game, 'Games als Fortnite zijn gratis, en toch geven mensen er meer geld aan uit dan een nieuwe game?!', 520, 290, 100, 500));
+    this.speedBubble.push(new SpeedBubble(game, 'Door te gamen leren kinderen 23% effectiever dan boeken te lezen!', 520, 290, 100, 500));
   }
 
   /**
