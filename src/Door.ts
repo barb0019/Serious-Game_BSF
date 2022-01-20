475487e9d3c192a9b8f1b37465446dc75fd42886
schl0014 <schl0014@hz.nl>
2022-01-20 14:18:55 +0100

    update door

--- src/Door.ts
+++ src/Door.ts
@@ -11,7 +11,7 @@ export default class Door extends InteractingItems {
    * @param type type of the door
    */
   public constructor(xPos: number, yPos: number, type:string) {
-    super(xPos, yPos, type, `./assets/img/${type}.png`);
+    super(xPos, yPos, type, `./assets/img/door/${type}.png`);
   }
 
   /**
@@ -22,9 +22,9 @@ export default class Door extends InteractingItems {
    */
   public draw(ctx: CanvasRenderingContext2D, player:Player[]): void {
     if (player[0].collidesWith(this) && player[1].collidesWith(this)) {
-      ctx.drawImage(Game.loadNewImage('./assets/img/DoubleDoor2.png'), this.xPos, this.yPos, 75, 100);
+      ctx.drawImage(Game.loadNewImage('./assets/img/door/DoubleDoor2.png'), this.xPos, this.yPos, 75, 100);
     } else if (player[0].collidesWith(this) || player[1].collidesWith(this)) {
-      ctx.drawImage(Game.loadNewImage('./assets/img/DoubleDoor1.png'), this.xPos, this.yPos, 75, 100);
+      ctx.drawImage(Game.loadNewImage('./assets/img/door/DoubleDoor1.png'), this.xPos, this.yPos, 75, 100);
     } else {
       ctx.drawImage(this.img, this.xPos, this.yPos, 75, 100);
     }
