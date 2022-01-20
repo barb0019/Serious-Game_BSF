93b307af01abc09ecc0d3766d115d2e65a3992fe
barb0019 <barb0019@hz.nl>
2022-01-20 15:17:34 +0100

    tty
    yy

--- src/Level4.ts
+++ src/Level4.ts
@@ -46,9 +46,9 @@ export default class Level4 extends Level {
    */
   protected speedbubbles(game: Game): void {
     this.speedBubble = [];
-    this.speedBubble.push(new SpeedBubble(game, 'in-game aankopen zijn niet altijd slecht', 230, 260, 100, 500));
-    this.speedBubble.push(new SpeedBubble(game, 'Soms maken ze de game-ervaring leuker', 50, 1150, 100, 500));
-    this.speedBubble.push(new SpeedBubble(game, 'Maar let wel op wat je uitgeeft!!', 350, 1250, 100, 500));
+    this.speedBubble.push(new SpeedBubble(game, '2 op de 5 kinderen koopt wekelijks iets in een game.', 230, 260, 100, 500));
+    this.speedBubble.push(new SpeedBubble(game, 'De helft van alle ouders weten niks over hetgame gedrag zijn hun kinderen', 50, 1150, 100, 500));
+    this.speedBubble.push(new SpeedBubble(game, 'Wist je dat FUT packs binnenkort misschien worden weggehaald', 350, 1250, 100, 500));
   }
 
   /**
